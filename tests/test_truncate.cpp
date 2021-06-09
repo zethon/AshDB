a61965737d1bb88b4a1a18f60684005c6f441aa6
Addy <aclaure@gmail.com>
2021-06-08 20:59:30 -0400

    added a truncation that happens on a file boundary

--- tests/test_truncate.cpp
+++ tests/test_truncate.cpp
@@ -24,6 +24,7 @@ BOOST_AUTO_TEST_CASE(simple_truncate)
 
     project::PersonDB::Batch batch;
 
+    // create the initial 100 records
     for (auto i = 0u; i < 100; ++i)
     {
         project::Person p = project::Person::CreatePerson(i);
@@ -32,10 +33,13 @@ BOOST_AUTO_TEST_CASE(simple_truncate)
     BOOST_TEST(db->write(batch) == ashdb::WriteStatus::OK);
     BOOST_TEST(db->size() == 100);
 
+    // delete half, with a max filesize of 1024, this means the record is
+    // in the middle of a data file
     db->truncate(50);
     BOOST_TEST(db->size() == 50);
     BOOST_CHECK_THROW(auto temp = db->read(75), std::runtime_error);
 
+    // write a bunch of new records
     batch.clear();
     for (auto i = 0u; i < 50; ++i)
     {
@@ -45,6 +49,7 @@ BOOST_AUTO_TEST_CASE(simple_truncate)
     BOOST_TEST(db->write(batch) == ashdb::WriteStatus::OK);
     BOOST_TEST(db->size() == 100);
 
+    // make sure the original 50 records are still valid
     for (auto i = 0u; i < 50; ++i)
     {
         project::Person testp = project::Person::CreatePerson(i);
@@ -52,9 +57,65 @@ BOOST_AUTO_TEST_CASE(simple_truncate)
         BOOST_TEST((testp == db_p));
     }
 
+    // and validate the new 50 records
     for (auto i = 50u; i < 100; ++i)
     {
         project::Person testp = project::Person::CreatePerson((i-50)*2);
+        project::Person db_p = db->read(i);
+        BOOST_TEST((testp == db_p));
+    }
+}
+
+BOOST_AUTO_TEST_CASE(boundary)
+{
+    auto tempFolder = ashdb::test::tempFolder("simple_truncate").string();
+
+    ashdb::Options options;
+    options.filesize_max = 1024;
+
+    auto db = std::make_unique<project::PersonDB>(tempFolder, options);
+    BOOST_TEST(db->open() == ashdb::OpenStatus::OK);
+
+    project::PersonDB::Batch batch;
+
+    // create the initial 100 records
+    for (auto i = 0u; i < 100; ++i)
+    {
+        project::Person p = project::Person::CreatePerson(i);
+        batch.push_back(p);
+    }
+    BOOST_TEST(db->write(batch) == ashdb::WriteStatus::OK);
+    BOOST_TEST(db->size() == 100);
+
+    // with 1024 max filesize, this will means that record 45 is the
+    // first in the third datafile, which means we should only two
+    // after truncating the db
+    db->truncate(45);
+    BOOST_TEST(db->size() == 45);
+    BOOST_CHECK_THROW(auto temp = db->read(75), std::runtime_error);
+
+    // write a bunch of new records
+    batch.clear();
+    for (auto i = 0u; i < 10; ++i)
+    {
+        project::Person p = project::Person::CreatePerson(i*2);
+        batch.push_back(p);
+    }
+    BOOST_TEST(db->write(batch) == ashdb::WriteStatus::OK);
+    BOOST_TEST(db->size() == 55);
+
+    // make sure the original 45 records are still valid
+    for (auto i = 0u; i < 45; ++i)
+    {
+        project::Person testp = project::Person::CreatePerson(i);
+        project::Person db_p = db->read(i);
+        BOOST_TEST((testp == db_p));
+    }
+
+    // and validate the new 10 records
+    for (auto i = 45; i < 55; ++i)
+    {
+        project::Person testp = project::Person::CreatePerson((i-45)*2);
         project::Person db_p = db->read(i);
         BOOST_TEST((testp == db_p));
     }
