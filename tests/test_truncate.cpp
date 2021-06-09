af8dae748a5c173f46b0fc7b5b6553442b0f25b0
Addy <aclaure@gmail.com>
2021-06-08 21:04:31 -0400

    change test folder;

--- tests/test_truncate.cpp
+++ tests/test_truncate.cpp
@@ -68,7 +68,7 @@ BOOST_AUTO_TEST_CASE(simple_truncate)
 
 BOOST_AUTO_TEST_CASE(boundary)
 {
-    auto tempFolder = ashdb::test::tempFolder("simple_truncate").string();
+    auto tempFolder = ashdb::test::tempFolder("boundary").string();
 
     ashdb::Options options;
     options.filesize_max = 1024;
